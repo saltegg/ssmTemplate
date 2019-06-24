88a097b4437e8ba53a15838fe4a6399286889b45
saltegg <805299748@qq.com>
2019-06-24 15:00:10 +0800

    test

---
+++ src/main/java/com/ssm/controller/PageController.java
@@ -0,0 +1,17 @@
+package com.ssm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+    @RequestMapping("/")
+    public String showIndex(){
+        return null;
+    }
+    @RequestMapping("/{page}")
+    public String showPage(@PathVariable String page){
+        return page;
+    }
+}
